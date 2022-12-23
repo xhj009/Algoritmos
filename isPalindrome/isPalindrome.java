4002a4bf0030902a78f450c8603dea16b7ab18ad
xhj <N@n@ing059>
2022-12-23 18:49:03 +0100

    Palindromo

---
+++ isPalindrome/isPalindrome.java
@@ -0,0 +1,23 @@
+package isPalindrome;
+
+public class isPalindrome {
+    public static boolean isPalindrome(int x) {
+        String numero = String.valueOf(x);
+        String numeroAlReves = "";
+
+        for (int i = numero.length() -1 ; i >= 0 ; i--) {
+            numeroAlReves += numero.charAt(i);
+            if (numeroAlReves.equals(numero)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int x = 121;
+        System.out.println(isPalindrome(x));
+    }
+}
+
+
